0577a379fc57ae87dc1f38a8134111484c3f10e9
Robert Dey <robdey88@gmail.com>
2018-07-09 10:40:11 +0200

    add spring boot project

---
+++ src/main/java/de/robertdey/kitchenbob/KitchenbobApplication.java
@@ -0,0 +1,12 @@
+package de.robertdey.kitchenbob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenbobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenbobApplication.class, args);
+    }
+}

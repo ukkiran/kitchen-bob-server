30fe2f78af39e29954f13bda468b7c75befbcc2f
Robert Dey <robdey88@gmail.com>
2018-07-09 15:29:44 +0200

    add additional find methods to the repositories

--- src/main/java/de/robertdey/kitchenbob/repositories/CategoryRepository.java
+++ src/main/java/de/robertdey/kitchenbob/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package de.robertdey.kitchenbob.repositories;
 import de.robertdey.kitchenbob.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/de/robertdey/kitchenbob/repositories/UnitOfMeasureRepository.java
+++ src/main/java/de/robertdey/kitchenbob/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package de.robertdey.kitchenbob.repositories;
 import de.robertdey.kitchenbob.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }

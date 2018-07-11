a8dc7e92ab5396ba8a503aa91467a9bd5dca37c2
Robert Dey <robdey88@gmail.com>
2018-07-11 17:09:43 +0200

    add some very basic form validation

--- src/main/java/de/robertdey/kitchenbob/commands/RecipeCommand.java
+++ src/main/java/de/robertdey/kitchenbob/commands/RecipeCommand.java
@@ -4,7 +4,12 @@ import de.robertdey.kitchenbob.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,17 +18,41 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String title;
+
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+    @Min(1)
+    @Max(999)
     private Integer servings;
+
+    @Size(min = 3, max = 255)
     private String source;
+
+    @URL
+    @Size(min = 3, max = 255)
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
+
     private Difficulty difficulty;
+
     private NotesCommand notes;
+
     private Set<CategoryCommand> categories = new HashSet<>();
+
     private Byte[] image;
 }
--- src/main/java/de/robertdey/kitchenbob/controllers/RecipeController.java
+++ src/main/java/de/robertdey/kitchenbob/controllers/RecipeController.java
@@ -8,13 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class RecipeController {
+
+    private static final String RECIPE_FORM_URL = "recipe/recipeform";
 
     private final RecipeService recipeService;
 
@@ -31,17 +36,27 @@ public class RecipeController {
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
-        return "recipe/recipeform";
+        return RECIPE_FORM_URL;
     }
 
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
+        return RECIPE_FORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            bindingResult.getAllErrors().forEach(objectError ->
+                log.debug(objectError.toString())
+            );
+
+            return RECIPE_FORM_URL;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
